1205f339846ccfb393176c7350fd46a4112afd3d
Nishibayashi Takuji <takuji@senchan.jp>
2011-02-17 22:35:06 +0900

    initial commit

---
+++ lib/Acme/QBe.pm
@@ -0,0 +1,32 @@
+package Acme::QBe;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Acme::QBe -
+
+=head1 SYNOPSIS
+
+  use Acme::QBe;
+
+=head1 DESCRIPTION
+
+Acme::QBe is
+
+=head1 AUTHOR
+
+Nishibayashi Takuji E<lt>takuji {at} senchan.jpE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
